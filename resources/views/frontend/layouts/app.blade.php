89d3916fbf52433386dc9bd11f2545301f4fbc59
golz04 <muhammadhaidar862@gmail.com>
2023-04-12 23:30:06 +0700

    first commit -m setup frontend

---
+++ resources/views/frontend/layouts/app.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <title>Inner Page - BizLand Bootstrap Template</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+
+    <link href="{{ asset('assets/frontend/img/favicon.png') }}" rel="icon">
+    <link href="{{ asset('assets/frontend/img/apple-touch-icon.png') }}" rel="apple-touch-icon">
+
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Roboto:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rels="stylesheet">
+
+    <link href="{{ asset('assets/frontend/vendor/aos/aos.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/frontend/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/frontend/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/frontend/vendor/boxicons/css/boxicons.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/frontend/vendor/glightbox/css/glightbox.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/frontend/vendor/swiper/swiper-bundle.min.css') }}" rel="stylesheet">
+
+    <link href="{{ asset('assets/frontend/css/style.css') }}" rel="stylesheet">
+</head>
+
+<body>
+    @include('frontend.components.topbar')
+    @include('frontend.components.header')
+    <main id="main" data-aos="fade-up">
+        @yield('content')
+    </main>
+
+    @include('frontend.components.footer')
+
+    <div id="preloader"></div>
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+    <script src="{{ asset('assets/frontend/vendor/purecounter/purecounter_vanilla.js') }}"></script>
+    <script src="{{ asset('assets/frontend/vendor/aos/aos.js') }}"></script>
+    <script src="{{ asset('assets/frontend/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('assets/frontend/vendor/glightbox/js/glightbox.min.js') }}"></script>
+    <script src="{{ asset('assets/frontend/vendor/isotope-layout/isotope.pkgd.min.js') }}"></script>
+    <script src="{{ asset('assets/frontend/vendor/swiper/swiper-bundle.min.js') }}"></script>
+    <script src="{{ asset('assets/frontend/vendor/waypoints/noframework.waypoints.js') }}"></script>
+    <script src="{{ asset('assets/frontend/vendor/php-email-form/validate.js') }}"></script>
+    <script src="{{ asset('assets/frontend/js/main.js') }}"></script>
+</body>
+</html>
